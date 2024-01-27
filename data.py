179de357e4c9eb4f709b454ab1c303c6249a6e29
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-27 19:44:29 +0400

    update 5.2

--- data.py
+++ data.py
@@ -183,6 +183,7 @@ class Data:
         with self.connect:
             self.cursor.execute("SELECT * FROM chats WHERE user_first=%s OR user_second=%s", (user_id, user_id,))
             result = self.cursor.fetchall()[0]
+            result = [item for item in result]
             print(result)
 
     def add_queue_female(self, user_id):
