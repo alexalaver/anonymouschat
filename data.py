4c9bcc33eb1c8345a89fdfeb36f36e8e3d1949d2
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-27 19:45:24 +0400

    update 5.3

--- data.py
+++ data.py
@@ -179,7 +179,7 @@ class Data:
                 print(f"RIGHT 2 {self.cursor.fetchone()[0]}")
                 return self.cursor.fetchone()[0]
 
-    def test_sa(self, user_id):
+    def get_full_chats_info(self, user_id):
         with self.connect:
             self.cursor.execute("SELECT * FROM chats WHERE user_first=%s OR user_second=%s", (user_id, user_id,))
             result = self.cursor.fetchall()[0]
