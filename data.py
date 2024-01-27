225e50a046f96d29242e246806974c60e0c1fe11
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-27 18:42:18 +0400

    update 4.0

--- data.py
+++ data.py
@@ -161,10 +161,11 @@ class Data:
 
     def select_serach_gender(self, user_id):
         with self.connect:
-            self.cursor.execute("SELECT search_gender_first FROM chats WHERE user_first=%s", (int(user_id),))
+            self.cursor.execute("SELECT search_gender_first FROM chats WHERE user_first=%s", (user_id,))
             search_gender_first = self.cursor.fetchone()
+            print(search_gender_first)
             if search_gender_first is None:
-                self.cursor.execute("SELECT search_gender_second FROM chats WHERE user_second=%s", (int(user_id),))
+                self.cursor.execute("SELECT search_gender_second FROM chats WHERE user_second=%s", (user_id),)
                 search_gender_second = self.cursor.fetchone()
                 if search_gender_second is None:
                     return None
