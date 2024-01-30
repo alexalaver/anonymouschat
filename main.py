67e15148e75f574b623f070a921166c686072107
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-30 14:18:00 +0400

    add supports 1.0

--- main.py
+++ main.py
@@ -427,14 +427,14 @@ async def supports_user_send_support_func(message, state):
     else:
         if message.text:
             await bot.send_message(cfg.supports_group_tag, f"{cfg.USER_SEND_TASK_TEXT(user=fnc.nick_with_link('Օգտատերը', user_id), user_id=user_id)}\n\n{message.text}", parse_mode=types.ParseMode.MARKDOWN)
-            await message.answer(cfg.support_user_send_sup_text, reply_markup=markup)
+            await message.answer(cfg.support_user_send_sup_text)
         elif message.photo:
             if message.caption:
                 await bot.send_photo(cfg.supports_group_tag, caption=f"{cfg.USER_SEND_TASK_TEXT(user=fnc.nick_with_link('Օգտատերը', user_id), user_id=user_id)}\n\n{message.caption}", photo=message.photo[0].file_id, parse_mode=types.ParseMode.MARKDOWN)
-                await message.answer(cfg.support_user_send_sup_text, reply_markup=markup)
+                await message.answer(cfg.support_user_send_sup_text)
             else:
                 await bot.send_photo(cfg.supports_group_tag, caption=f"{cfg.USER_SEND_TASK_PHOTO_TEXT(user=fnc.nick_with_link('Օգտատերը', user_id), user_id=user_id)}", photo=message.photo[0].file_id, parse_mode=types.ParseMode.MARKDOWN)
-                await message.answer(cfg.support_user_send_sup_text, reply_markup=markup)
+                await message.answer(cfg.support_user_send_sup_text)
         else:
             await message.answer(cfg.support_user_send_sup_error)
 
