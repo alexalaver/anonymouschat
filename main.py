58b339c09746507e77021c943bb8e7f107ff1343
alexalaver <alex.alaverdyan.04@gmail.com>
2024-07-13 02:06:48 +0400

    beta channels 3.0

--- main.py
+++ main.py
@@ -598,18 +598,19 @@ async def reg_2_text(message: types.Message):
 
 ######################### ADD CHANNELS FUNC
 
-async def get_channel_id(channel: str) -> int:
+async def get_chat_id(channel: str) -> int:
     try:
-        # Используйте имя канала без префикса URL
+        # Удаление префиксов и получение информации о чате
         channel_name = channel.replace("https://t.me/", "").replace("@", "")
         chat = await bot.get_chat(channel_name)
         return chat.id
     except Exception as e:
-        print(f"Error getting channel ID for {channel}: {e}")
+        print(f"Error getting chat ID for {channel}: {e}")
+        return None
 
 async def check_if_admin(channel: str) -> bool:
     try:
-        channel_id = await get_channel_id(channel)
+        channel_id = await get_chat_id(channel)
         if channel_id:
             admins = await bot.get_chat_administrators(channel_id)
             for admin in admins:
@@ -622,7 +623,7 @@ async def check_if_admin(channel: str) -> bool:
 
 async def check_if_member(channel: str, user_id: int) -> bool:
     try:
-        channel_id = await get_channel_id(channel)
+        channel_id = await get_chat_id(channel)
         if channel_id:
             member = await bot.get_chat_member(channel_id, user_id)
             return member.status != types.ChatMemberStatus.LEFT
