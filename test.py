c3061624f15ae3eb5a9e1e6670f8820e75ed5901
alexalaver <alex.alaverdyan.04@gmail.com>
2024-07-21 01:01:11 +0400

    realize 1.7

--- test.py
+++
@@ -1,6 +0,0 @@
-try:
-    user_id = message.from_user.id
-    await message.answer(cfg.USER_LOGS_COM(fnc.nick_with_link("Օգտագործողը", user_id), "/start"))
-except Exception as err:
-    error_message = f"An error occurred: {err}\n" + traceback.format_exc()
-    await bot.send_message(chat_id=cfg.logs_group, text=f"An unexpected error occurred:\n{error_message}")
